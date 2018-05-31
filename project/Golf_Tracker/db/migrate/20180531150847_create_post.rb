67d065a06731b3c5ea2f3bb5a6e23a77310c81ae
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-05-31 08:13:50 -0700

    create_table code.

--- project/Golf_Tracker/db/migrate/20180531150847_create_post.rb
+++ project/Golf_Tracker/db/migrate/20180531150847_create_post.rb
@@ -1,4 +1,10 @@
 class CreatePost < ActiveRecord::Migration
   def change
+      create_table :posts do |t|
+          t.string :course
+          t.integer :score
+          t.string :date
+          t.integer :user_id
+      end
   end
 end

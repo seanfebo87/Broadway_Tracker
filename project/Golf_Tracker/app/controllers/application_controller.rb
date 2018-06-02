b9110f5416db228e1a0370ce6c5d44b871493ffb
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-02 14:23:13 -0700

    created get request for logging out.

--- project/Golf_Tracker/app/controllers/application_controller.rb
+++ project/Golf_Tracker/app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
             @user = User.create(email: params[:email], password: params[:password])
             @user.save
             session[:user_id] = @user.id
-            redirect to '/login'
+            redirect to '/homepage'
         end
     end
 
@@ -61,6 +61,15 @@ class ApplicationController < Sinatra::Base
         if logged_in?
             @user = current_user
             erb :homepage
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.clear
+            redirect to 'login'
         else
             redirect to '/login'
         end

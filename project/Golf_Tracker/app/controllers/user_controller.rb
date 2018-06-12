018f42c4e4d81896168340e4c2c2c21e3f440f1e
Sean Febo <sfeebs@Seans-MacBook-Pro.local>
2018-06-12 13:20:22 -0700

    moved user code to User controller.

---
+++ project/Golf_Tracker/app/controllers/user_controller.rb
@@ -0,0 +1,13 @@
+require './config/environment'
+require 'rack-flash'
+class UserController < ApplicationController
+use Rack::Flash
+
+    get '/user/:id' do
+            @user = User.find(params[:id])
+            if logged_in? && @user && @user.id == session[:user_id]
+            erb :'/users/show'
+        else
+            redirect to '/'
+        end
+    end
